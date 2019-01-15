be87d64530f91ce858b61c242c36b91dbcb8bd08
cezar77 <cezarpendarovski@web.de>
2019-01-15 13:34:08 +0000

    Clean up app.js

--- src/app.js
+++ src/app.js
@@ -19,6 +19,10 @@ angular
       length = cards.length
     ;
 
+    $scope.shuffle = function() {
+      return 0.5 - Math.random();
+    };
+
     $scope.deck = [];
 
     for (i = 0; i < length; i++) {
@@ -37,7 +41,7 @@ angular
         'removed': false
       });
     }
-    $scope.deck.sort(shuffle);
+    $scope.deck.sort($scope.shuffle);
     $scope.deck.forEach(function(element, index) {
       element.position = index;
     });
@@ -55,7 +59,6 @@ angular
       if ($scope.selectedCards.length == 2) {
         $timeout($scope.checkPattern, 1500);
       }
-      console.log($scope.selectedCards);
     };
 
     $scope.checkPattern = function() {
@@ -70,64 +73,3 @@ angular
     };
   })
 ;
-
-function shuffle() {
-  return 0.5 - Math.random();
-}
-
-function selectCard() {
-  if ($('.card-flipped').length > 1) {
-    return;
-  }
-  $(this).addClass('card-flipped');
-  if ($('.card-flipped').length == 2) {
-    setTimeout(checkPattern, 700);
-  }
-}
-
-function checkPattern() {
-  if (isMatchPattern()) {
-    $('.card-flipped').removeClass('card-flipped').addClass('card-removed');
-    $('.card-removed').bind('webkitTransitionEnd', removeTookCards);
-  } else {
-    $('.card-flipped').removeClass('card-flipped');
-  }
-}
-
-function isMatchPattern() {
-  var cards = $('.card-flipped');
-  var pattern = $(cards[0]).data('pattern');
-  var anotherPattern = $(cards[1]).data('pattern');
-  return (pattern == anotherPattern);
-}
-
-function removeTookCards() {
-  $('.card-removed').remove();
-}
-
-//var matchingGame = {};
-//matchingGame.deck = [
-//  'cardAK', 'cardAK',
-//  'cardAQ', 'cardAQ',
-//  'cardAJ', 'cardAJ',
-//  'cardBK', 'cardBK',
-//  'cardBQ', 'cardBQ',
-//  'cardBJ', 'cardBJ',
-//];
-
-//$(function(){
-//matchingGame.deck.sort(shuffle);
-//for (var i = 0; i < 11; i++) {
-//  $('.card:first-child').clone().appendTo('#cards');
-//}
-//  $("#cards").children().each(function(index) {
-//    $(this).css({
-//      'left': ($(this).width() + 20) * (index % 4),
-//      'top': ($(this).height() + 20) * Math.floor(index / 4)
-//    });
-//    var pattern = $('.card').pop();
-//    $(this).find('.back').addClass(pattern.name);
-//    $(this).attr('data-pattern', pattern.name);
-//    $(this).click(selectCard);
-//  });
-//});
